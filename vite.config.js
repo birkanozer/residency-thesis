f4dc005cfcaf0be3799f22741d1966edd0b1d052
Birkan Özer <birkanozer@hotmail.com>
2023-09-01 21:19:53 +0300

    gh pages forth attempt

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/residency-thesis/",
   plugins: [react()],
   server: {
     watch: {
