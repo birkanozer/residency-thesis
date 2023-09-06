131e49cd2912b95e213ab01b4700a7f4691d178e
Birkan Özer <birkanozer@hotmail.com>
2023-09-06 10:56:35 +0300

    SSG trial

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ssr } from "vite-plugin-ssr/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/residency-thesis/",
-  plugins: [react()],
+  plugins: [react(), ssr({ prerender: true })],
   server: {
     watch: {
       usePolling: true
