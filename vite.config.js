74b3f070d07b3e75f6c5f5102f9eb2046c9906bb
Birkan Özer <birkanozer@hotmail.com>
2023-09-06 11:18:09 +0300

    fixes and stabilization

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ssr } from "vite-plugin-ssr/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/residency-thesis/",
-  plugins: [react(), ssr({ prerender: true })],
+  plugins: [react()],
   server: {
     watch: {
       usePolling: true
